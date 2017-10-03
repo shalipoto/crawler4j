7ac3f9660e05c1dd3990043e49aec22ce1ed5c6a
Saleem Halipoto <shalipoto@gmail.com>
2017-10-03 16:14:19 -0500

    Adjusted crawl depth and seeds to keep things simple.

--- crawler4j/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawlController.java
+++ crawler4j/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawlController.java
@@ -66,7 +66,7 @@ public class BasicCrawlController {
      * You can set the maximum crawl depth here. The default value is -1 for
      * unlimited depth
      */
-        config.setMaxDepthOfCrawling(2);
+        config.setMaxDepthOfCrawling(1);
 
     /*
      * You can set the maximum number of pages to crawl. The default value
@@ -112,8 +112,8 @@ public class BasicCrawlController {
      * which are found in these pages
      */
         controller.addSeed("http://www.ics.uci.edu/");
-        controller.addSeed("http://www.ics.uci.edu/~lopes/");
-        controller.addSeed("http://www.ics.uci.edu/~welling/");
+        /*controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        controller.addSeed("http://www.ics.uci.edu/~welling/");*/
 
     /*
      * Start the crawl. This is a blocking operation, meaning that your code
