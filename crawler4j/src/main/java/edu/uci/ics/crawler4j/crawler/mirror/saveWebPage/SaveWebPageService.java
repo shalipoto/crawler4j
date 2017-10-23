e597f17e207cd873d1c818b84c7d0a23237962c3
Saleem Halipoto <shalipoto@crownpartners.com>
2017-10-23 07:57:26 -0500

    Added a second method to the SaveWebPageService interface and created the service implementation file.

--- crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/mirror/saveWebPage/SaveWebPageService.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/mirror/saveWebPage/SaveWebPageService.java
@@ -1,6 +1,16 @@
 package edu.uci.ics.crawler4j.crawler.mirror.saveWebPage;
 
+/**
+ * This interface provides the services for 
+ * the crawler to gather the files needed to 
+ * save a web page for offline viewing
+ * 
+ * @author saleemhalipoto
+ *
+ */
 public interface SaveWebPageService {
 
 	void SaveCompleteWebPage();
+	
+	void SaveHtmlOnly();
 }
---
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/mirror/saveWebPage/SaveWebPageServiceImpl.java
@@ -0,0 +1,17 @@
+package edu.uci.ics.crawler4j.crawler.mirror.saveWebPage;
+
+public class SaveWebPageServiceImpl implements SaveWebPageService{
+
+	@Override
+	public void SaveCompleteWebPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SaveHtmlOnly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
