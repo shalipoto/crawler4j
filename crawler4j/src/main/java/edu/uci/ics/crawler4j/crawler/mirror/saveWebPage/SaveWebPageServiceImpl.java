d3eecc8370e1cf888e8bb1ff0053f52021fa6688
Saleem Halipoto <shalipoto@gmail.com>
2017-10-25 00:01:18 -0500

    Added some comments to impl file and created a unit test class for it also.

--- crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/mirror/saveWebPage/SaveWebPageServiceImpl.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/mirror/saveWebPage/SaveWebPageServiceImpl.java
@@ -1,5 +1,13 @@
 package edu.uci.ics.crawler4j.crawler.mirror.saveWebPage;
 
+/**
+ * This implements the SaveWebPageService interface and 
+ * is intended to save a web page and its links for offline
+ * browsing
+ * 
+ * @author saleemhalipoto
+ *
+ */
 public class SaveWebPageServiceImpl implements SaveWebPageService{
 
 	@Override
