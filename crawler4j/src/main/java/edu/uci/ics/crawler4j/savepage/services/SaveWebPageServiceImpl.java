d9767d8099ac8b356afd8a2296c12dfa91d902f2
Saleem Halipoto <shalipoto@gmail.com>
2017-10-26 03:32:14 -0500

    Cleaned up comments.

--- crawler4j/src/main/java/edu/uci/ics/crawler4j/savepage/services/SaveWebPageServiceImpl.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/savepage/services/SaveWebPageServiceImpl.java
@@ -8,14 +8,14 @@ import edu.uci.ics.crawler4j.DTO.CompleteWebPageDTO;
  * local file system just as browser would when saving a 
  * complete web page.
  * 
- * @author saleemhalipoto
+ * @author Saleem Halipoto
  *
  */
 public class SaveWebPageServiceImpl implements SaveWebPageService{
 
 	
 	/**
-	 * @param pageDTO the DTO built by the WebCrawler instance
+	 * @param pageDTO the DTO instantiated and populated by the WebCrawler instance
 	 * @param location the crawlStorageFolder named by an argument to main()
 	 */
 	@Override
@@ -26,14 +26,12 @@ public class SaveWebPageServiceImpl implements SaveWebPageService{
 		// Get the list of support files for the CompleteWebPage
 		
 		// Save the list of support files to the local file system
-		
-		
+				
 	}
 
 	@Override
 	public void SaveHtmlOnly() {
-		// TODO Auto-generated method stub
-		
+		// TODO implement when needed	
 	}
 
 }
